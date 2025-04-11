bc3281509760744f7b1998172c3a4453bfb45840
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2025-04-12 05:31:40 +0700

    chore: cleanup

--- src/routes/batch.ts
+++ src/routes/batch.ts
@@ -1,20 +1,12 @@
 import { Router } from "express";
-import { getAll } from "../controllers/medicine";
 import verifyToken from "../middlewares/verifyToken";
 import zodSchemaValidator from "../lib/zodSchemaValidator";
-import { addProductSchema, editStockBatchSchema } from "../zodSchema/product";
+import { editStockBatchSchema } from "../zodSchema/product";
 import { edit } from "../controllers/batch";
 
 
 const StockBatchRouter = Router({ mergeParams: true, caseSensitive:true })
 
-// StockBatchRouter.get('/', getAll)
 StockBatchRouter.patch('/edit', verifyToken, zodSchemaValidator(editStockBatchSchema), edit)
-// StockBatchRouter.post('/add', zodSchemaValidator(addProductSchema), addProduct)
-// StockBatchRouter.post('/login', login)
-// StockBatchRouter.patch('/changePassword', verifyToken, changePassword)
-// StockBatchRouter.patch('/update', verifyToken, update)
-// StockBatchRouter.post('/logout', verifyToken, logout)
-// StockBatchRouter.delete('/delete', verifyToken, deleteOne)
 
 export default StockBatchRouter
--- src/routes/medicine.ts
+++ src/routes/medicine.ts
@@ -1,8 +1,5 @@
 import { Router } from "express";
 import { getAll } from "../controllers/medicine";
-import verifyToken from "../middlewares/verifyToken";
-import zodSchemaValidator from "../lib/zodSchemaValidator";
-import { addProductSchema } from "../zodSchema/product";
 
 
 const medicineRouter = Router({ mergeParams: true, caseSensitive:true })
